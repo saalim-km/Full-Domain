2ef24efefc36bd766ec96e365a4a0e0f04fdfcfe
saalim-km <saalimkm@gmail.com>
2025-02-09 14:01:23 +0530

    ...

--- Recursion/array.js
+++ Recursion/array.js
@@ -10,3 +10,12 @@ function findSum( num , result) {
 console.log(findSum(123,0));
 
 
+//? FIBONACCI SEQUENCE
+function fibonacci(n) {
+    if(n==0) return 0;
+    if(n==1) return 1;
+  
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
+  
+  console.log(fibonacci(5))
--- Recursion/string.js
+++ Recursion/string.js
@@ -1,4 +1,22 @@
 //? REVERSE STRING
-function reverseString(str ,start, end) {
-    
+function reverseString(str, index = 0, result = '') {
+    if(index === str.length) return result;
+
+    return str[str.length-1 - index] + reverseString(str, index + 1);
 }
+
+console.log(reverseString('hi'))
+
+
+//? DELETE LETTER FROM A STRING
+function deleteWord(str,index,result,target) {
+    if(index === str.length) return result;
+
+    if(str[index] !== target){
+        result += str[index];
+    }
+
+    return deleteWord(str,index+1,result,target);
+}
+
+console.log(deleteWord('hello',0,'','l'))
