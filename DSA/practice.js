8999f1cbda50ff6794c675b211a1554f0792bf40
saalim-km <saalimkm@gmail.com>
2025-02-17 09:39:31 +0530

    review day

--- DSA/practice.js
+++ DSA/practice.js
@@ -496,3 +496,112 @@
 // console.log(graph.dfs('a'))
 
 // console.log(graph)
+
+
+// class Node {
+//     constructor() {
+//         this.children = {};
+//         this.endOfWord = false;
+//     }
+// }
+
+// class Trie {
+//     constructor() {
+//         this.root = new Node();
+//     }
+
+//     insertWord(word) {
+//         let current = this.root;
+//         for(let char of word) {
+//             if(!current.children[char]) {
+//                 current.children[char] = new Node();
+//             }
+//             current = current.children[char];
+//         }
+
+//         current.endOfWord = true;
+//     }
+
+//     search(word) {
+//         let current = this.root;
+//         for(let char of word) {
+//             if(!current.children[char]) {
+//                 return false;
+//             }
+//             current = current.children[char];
+//         }
+//         return current.endOfWord;
+//     }
+
+//     remove(word) {
+//         function removeWordHelper(node,word,depth) {
+//             if(!node) {
+//                 return false;
+//             }
+
+//             if(depth === word.length) {
+//                 if(!node.endOfWord) {
+//                     return false;
+//                 }
+
+//                 node.endOfWord = false;
+//                 return Object.keys(node.children).length === 0;
+//             }
+
+//             const char = word[depth];
+//             const shouldBeRemoved = removeWordHelper(
+//                 node.children[char],
+//                 word,
+//                 depth+1
+//             );
+
+//             if(shouldBeRemoved) {
+//                 delete node.children[char];
+//             }
+
+//             return Object.keys(node.children).length === 0;
+//         }
+
+//         removeWordHelper(this.root,word,0);
+//     }
+
+//     autoComplete(prefix) {
+//         const result = [];
+
+//         function dfs(node,path) {
+//             if(node.endOfWord) {
+//                 result.push(path);
+//             }
+
+//             for(let char in node.children) {
+//                 dfs(node.children[char] , path+=char);
+//             }
+//         }
+
+//         let current = this.root;
+//         for(let char of prefix) {
+//             if(!current.children[char]) {
+//                 return [];
+//             }
+//             current = current.children[char];
+//         }
+
+//         dfs(current,prefix);
+//         return result;
+//     }
+// }
+
+
+// const trie = new Trie();
+// trie.insertWord("nigga")
+// trie.insertWord("sayipp")
+// trie.insertWord("saloon")
+// trie.insertWord("salmaan")
+
+// // trie.remove("nigga")
+
+// console.log(trie.search("nigga"))
+// console.log(trie.autoComplete("sa"))
+
+
+// console.log(trie)
